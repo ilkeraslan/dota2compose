11bfe3dff99fd627cf676ec77eed9d9a7561540e
Ilker Aslan <ilkeer.ozgur@gmail.com>
2021-04-08 15:44:17 +0200

    Bump to Compose 1.0.0-beta04

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha10")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.33-beta")
     }
