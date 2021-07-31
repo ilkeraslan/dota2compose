328fdce3cb8baa64db517a77ffbdc861ba3b0fa7
Ilker Aslan <ilkerozguraslan@gmail.com>
2021-07-31 13:59:47 +0200

    Add Scrim to Bottomsheet

--- app/src/main/java/me/ilker/dota2compose/ui/components/BottomSheet.kt
+++ app/src/main/java/me/ilker/dota2compose/ui/components/BottomSheet.kt
@@ -3,56 +3,80 @@ package me.ilker.dota2compose.ui.components
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @Composable
 fun BottomSheet(
     modifier: Modifier = Modifier,
-    initialState: BottomSheetState = BottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    scope: CoroutineScope = rememberCoroutineScope(),
+    scaffoldState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = BottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    ),
+    content: @Composable () -> Unit
 ) {
-    val scope = rememberCoroutineScope()
-    val scaffoldState = rememberBottomSheetScaffoldState(
-        bottomSheetState = initialState
-    )
-
     BottomSheetScaffold(
         modifier = modifier,
         sheetContent = { BottomSheetContent() },
         scaffoldState = scaffoldState,
-        sheetPeekHeight = 0.dp
+        sheetPeekHeight = 0.dp,
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
     ) {
-
+        BoxWithConstraints(modifier) {
+            Box(Modifier.fillMaxSize()) {
+                content()
+                Scrim(
+                    onDismiss = {
+                        if (scaffoldState.bottomSheetState.targetValue == BottomSheetValue.Collapsed) {
+                            scope.launch { scaffoldState.bottomSheetState.collapse() }
+                        }
+                    },
+                    visible = scaffoldState.bottomSheetState.targetValue != BottomSheetValue.Collapsed
+                )
+            }
+        }
     }
 }
 
 @Composable
 private fun BottomSheetContent(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+    Column(
+        modifier = Modifier.padding(horizontal = 20.dp)
+    ) {
+        Spacer(modifier = Modifier.height(20.dp))
         Text(text = "Title")
         Spacer(modifier = Modifier.height(20.dp))
         Text(text = "A long warning description to make sure that the user knows what is going on")
         Spacer(modifier = Modifier.height(20.dp))
 
         Column(modifier = modifier.fillMaxWidth()) {
-            Button(
+            TextButton(
                 modifier = modifier.fillMaxWidth(),
                 onClick = { /*TODO*/ }
             ) {
                 Text(text = "OK")
             }
             Spacer(modifier = Modifier.height(10.dp))
-            Button(
+            TextButton(
                 modifier = modifier.fillMaxWidth(),
-                onClick = { /*TODO*/ }) {
+                onClick = { /*TODO*/ }
+            ) {
                 Text(text = "CANCEL")
             }
+            Spacer(modifier = Modifier.height(80.dp))
         }
     }
 }
@@ -70,5 +94,15 @@ private fun BottomSheetContent(modifier: Modifier = Modifier) {
 )
 @Composable
 fun BottomSheetPreview() {
-    BottomSheet(initialState = BottomSheetState(BottomSheetValue.Expanded))
+    BottomSheet(
+        scaffoldState = rememberBottomSheetScaffoldState(
+            bottomSheetState = BottomSheetState(initialValue = BottomSheetValue.Expanded)
+        ),
+        content = {
+            Text(
+                text = "Content Title",
+                fontSize = 20.sp
+            )
+        }
+    )
 }
--- app/src/main/java/me/ilker/dota2compose/ui/screens/TeamsScreen.kt
+++ app/src/main/java/me/ilker/dota2compose/ui/screens/TeamsScreen.kt
@@ -11,19 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import me.ilker.dota2compose.domain.Team
 import me.ilker.dota2compose.presenter.TeamsState
+import me.ilker.dota2compose.ui.components.BottomSheet
 
 @ExperimentalMaterialApi
 @Composable
@@ -38,9 +37,26 @@ fun TeamsScreen(
             teamsState.error.message,
             Toast.LENGTH_LONG
         ).show()
-        is TeamsState.Loaded -> LazyColumn(modifier = Modifier.padding(bottom = 60.dp)) {
-            items(teamsState.teams) { team ->
-                TeamCard(team = team)
+        is TeamsState.Loaded -> {
+            val scope = rememberCoroutineScope()
+            val scaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = BottomSheetState(initialValue = BottomSheetValue.Collapsed)
+            )
+
+            BottomSheet(
+                modifier = Modifier,
+                scope = scope,
+                scaffoldState = scaffoldState
+            ) {
+                LazyColumn(modifier = Modifier.padding(bottom = 60.dp)) {
+                    items(teamsState.teams) { team ->
+                        TeamCard(team = team) {
+                            scope.launch {
+                                scaffoldState.bottomSheetState.expand()
+                            }
+                        }
+                    }
+                }
             }
         }
         TeamsState.Loading -> Box { CircularProgressIndicator(Modifier.align(Alignment.Center)) }
@@ -49,13 +65,17 @@ fun TeamsScreen(
 
 @ExperimentalMaterialApi
 @Composable
-fun TeamCard(team: Team, modifier: Modifier = Modifier) {
+fun TeamCard(
+    team: Team,
+    modifier: Modifier = Modifier,
+    onClick : () -> Unit
+) {
     Card(
         modifier = modifier
             .padding(horizontal = 16.dp)
             .padding(top = 12.dp),
         backgroundColor = MaterialTheme.colors.background,
-        onClick = { Log.i("CLICK", "OK") }
+        onClick = { onClick() }
     ) {
         Column(
             modifier = Modifier
@@ -95,5 +115,5 @@ fun TeamCardPreview() {
             wins = 10
         ),
         modifier = Modifier
-    )
+    ) { /*No-op*/ }
 }
