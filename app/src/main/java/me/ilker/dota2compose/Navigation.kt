1c9703b2f40528ae5fe07a7ac37bc1be7fc77cb0
Ilker Aslan <ilkerozguraslan@gmail.com>
2022-07-11 07:34:57 +0200

    Change colors

--- app/src/main/java/me/ilker/dota2compose/Navigation.kt
+++ app/src/main/java/me/ilker/dota2compose/Navigation.kt
@@ -1,6 +1,7 @@
 package me.ilker.dota2compose
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -28,7 +29,7 @@ sealed class Screen(
     object TeamsScreen : Screen(
         route = "Teams",
         label = "Teams",
-        icon = Icons.Outlined.Warning
+        icon = Icons.Outlined.Build
     )
 }
 
