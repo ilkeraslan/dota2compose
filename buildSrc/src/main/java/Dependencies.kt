a78682115bae8ef5f852f73221e20029d8b38d4e
Ilker Aslan <ilkerozguraslan@gmail.com>
2022-10-09 10:42:46 +0200

    #32 Bump compose compiler to 1_3_2 (#33)
    Bump compose compiler to 1_3_2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,6 @@ object Versions {
     const val kotlin = "1.7.0"
     const val kotlinxCoroutines = "1.7.0"
 
-    const val compose = "1.0.0-beta02"
     const val nav_compose = "1.0.0-alpha09"
     const val nav_paging = "1.0.0-alpha08"
     const val accompanist = "0.6.2"
