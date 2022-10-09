4308222af1896ecec880902d0001f1ce84a158a4
Ilker Aslan <ilkerozguraslan@gmail.com>
2022-10-09 10:16:48 +0200

    Bump kotlin to 1_7_0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
     }
 }
 
