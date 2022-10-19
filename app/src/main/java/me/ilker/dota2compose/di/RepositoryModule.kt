94b34eae2eb4d3833baea16e9c4a5b7b5a3d7fdc
Indrajit Chakrabarty <indyfromoz@gmail.com>
2022-10-19 18:48:17 +1100

    feat: Added HeroesRepository and heroRankings call (#43)

--- app/src/main/java/me/ilker/dota2compose/di/RepositoryModule.kt
+++ app/src/main/java/me/ilker/dota2compose/di/RepositoryModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
+import me.ilker.dota2compose.repository.HeroesRepository
+import me.ilker.dota2compose.repository.HeroesRepositoryImpl
 import me.ilker.dota2compose.repository.LeaguesRepository
 import me.ilker.dota2compose.repository.LeaguesRepositoryImpl
 import me.ilker.dota2compose.repository.PlayersRepository
@@ -21,4 +23,8 @@ abstract class RepositoryModule {
     @Binds
     @ViewModelScoped
     abstract fun bindsPlayersRepository(repositoryImpl: PlayersRepositoryImpl): PlayersRepository
+    
+    @Binds
+    @ViewModelScoped
+    abstract fun bindsHeroesRepository(repositoryImpl: HeroesRepositoryImpl): HeroesRepository
 }
--- app/src/main/java/me/ilker/dota2compose/service/NetworkService.kt
+++ app/src/main/java/me/ilker/dota2compose/service/NetworkService.kt
@@ -1,11 +1,13 @@
 package me.ilker.dota2compose.service
 
+import me.ilker.dota2compose.model.network.response.HeroRankingsResponse
 import me.ilker.dota2compose.model.network.response.HeroResponse
 import me.ilker.dota2compose.model.network.response.TeamHeroResponse
 import me.ilker.dota2compose.model.network.response.TeamPlayerResponse
 import me.ilker.dota2compose.model.network.response.TeamResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface NetworkService {
 
@@ -24,4 +26,9 @@ interface NetworkService {
     suspend fun getTeamHeroes(
         @Path("team_id") teamID: String
     ): List<TeamHeroResponse>
+    
+    @GET("rankings")
+    suspend fun getHeroRankings(
+        @Query("hero_id") heroID: Int
+    ): HeroRankingsResponse
 }
