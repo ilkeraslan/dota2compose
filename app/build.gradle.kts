f0f054d9751e93086d61503bef0f7ba09fbc0f0a
ilkeraslan <ilkerozguraslan@gmail.com>
2025-04-13 22:23:49 +0200

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "me.ilker.dota2composer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
